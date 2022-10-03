5588f9b819197af3378f7d05a3e3356b51120b65
babluroy <bablu.roy@batchservice.com>
2022-10-04 02:33:24 +0530

    new routes added

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,5 +1,6 @@
 const {body, validationResult} = require("express-validator");
 const Blog = require("../models/blog");
+const Category = require("../models/category");
 const firebaseAdmin = require("firebase-admin");
 const  { v4: uuidv4 } = require("uuid")
 const formidable =  require("formidable");
@@ -50,6 +51,119 @@ exports.createBlog = (req,res) => {
     })
 }
 
+exports.getAllBlogs = (req,res) => {
+    let limit = req.query.limit ? parseInt(req.query.limit) : 5;
+    let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
+    Blog
+    .find()
+    .populate("category")
+    .sort([[sortBy, "asc"]])
+    .limit(limit)
+    .exec((err, blogs) => {
+        if(err){
+            return res.status(400).json({
+                error: "No blogs found"
+            })
+        }
+        res.status(200).json(blogs);
+    })
+}
+
+exports.getBlogById = (req, res, next, id) => {
+    Blog
+    .findById(id)
+    .populate("category")
+    .exec((err, blog) => {
+        if(err){
+            return res.status(400).json({
+                error: "Blog not found"
+            })
+        }
+        req.blog = blog;
+        next();
+    })
+}
+
+exports.getBlog = (req, res) => {
+    return res.json(req.blog);
+}
+
+exports.getAllCategories = (req, res) => {
+    Category
+    .find()
+    .populate()
+    .exec((err, category) => {
+        if(err) {
+            return res.status(400).json({
+                error: "No categories found"
+            })
+        }
+        return res.json(category);
+    })
+}
+
+
+exports.deleteBlog = (req, res) => {
+    let blog = req.blog;
+    blog.remove((err, deleteBlog) => {
+        if(err) {
+            return res.status(400).json({
+                error: "Failed to delete the blog"
+            })
+        }
+        res.json({
+            message: "Successfully deleted the blog"
+        })
+    })
+}
+
+exports.updateBlog = (req, res) => {
+    // let form = new formidable.IncomingForm();
+    // form.keepExtensions = true;
+
+    // form.parse(req, async (err, fields, file) => {
+    //     if(err){
+    //         return res.status(400).json({
+    //             error: "Issue with the image file"
+    //         })
+    //     }
+    //     const {title,category,desc} = fields;
+
+    //     // if(!title || !category || !desc) {
+    //     //     return res.status(400).json({
+    //     //         error: "Please fill-up all the fields"
+    //     //     })
+    //     // }
+
+    //     let blog = new Blog(fields);
+        
+    //     if(file.image || !file.image) {
+
+    //     if(file.image) {
+    //         if(file.image.size > 5000000){
+    //             return res.status(400).json({
+    //                 error: "Image is larger than 5MB"
+    //             })
+    //         }
+    //         const newFileName = new Date() + file.image.originalFilename
+    //         var isFileUploaded = await uploadFile(file.image.filepath, newFileName);
+    //     }
+
+    //         if(isFileUploaded || !isFileUploaded) {
+    //             if(isFileUploaded) {
+    //               var imageUrl = isFileUploaded[0].metadata.mediaLink;
+    //               blog.image = imageUrl;
+    //             }
+    //             blog.save((err, blog)=>{
+    //                 if(err) {
+    //                     return res.status(400).json({error: "Error while saving data"});
+    //                 }
+    //                 return res.status(400).json({success: "Successfully uploaded the blog"});
+    //             })
+    //         }
+    //     }
+    // })
+}
 
 const uploadFile = async (path, filename) => {
     return storageRef.upload(path, {
--- routes/blog.js
+++ routes/blog.js
@@ -1,13 +1,25 @@
 var express = require("express");
 var router = express.Router();
-const {isSignedIn,isAuthenticated} = require("../controllers/auth")
+const {isSignedIn,isAuthenticated, isAdmin} = require("../controllers/auth")
 const {getUserById} = require("../controllers/user")
-const {createBlog} = require("../controllers/blog")
+const {createBlog, getAllBlogs, getBlogById, getBlog, getAllCategories, deleteBlog, updateBlog} = require("../controllers/blog")
 
 // params
 router.param("userId", getUserById);
+router.param("blogId", getBlogById);
 
-router.post("/create-blog/:userId", isSignedIn, isAuthenticated, createBlog)
+router.post("/create-blog/:userId", isSignedIn, isAuthenticated, createBlog);
+
+router.get("/all-blogs", isSignedIn, getAllBlogs);
+
+router.get("/blog/:blogId", getBlog);
+
+router.get("/categories", getAllCategories);
+
+router.delete("/delete-blog/:blogId/:userId", isSignedIn, isAuthenticated, deleteBlog);
+
+// TODO: test it
+router.put("/updateBlog/:blogId/:userId", isSignedIn, isAuthenticated, updateBlog)
 
 
 module.exports = router;
